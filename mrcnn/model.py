e8b26d89a5895b2ad09d59616dce9c2e3f664059
SambitPrabhu <76251020+SambitPrabhu@users.noreply.github.com>
2023-11-05 04:22:29 +0530

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2174,7 +2174,7 @@ class MaskRCNN():
         loss_names = [
             "rpn_class_loss",  "rpn_bbox_loss",
             "mrcnn_class_loss", "mrcnn_bbox_loss", "mrcnn_mask_loss"]
-	      log("add layer losses to model")
+        log("add layer losses to model")
         for name in loss_names:
             layer = self.keras_model.get_layer(name)
             if layer.output in self.keras_model.losses:
@@ -2183,7 +2183,7 @@ class MaskRCNN():
                 tf.reduce_mean(layer.output, keepdims=True)
                 * self.config.LOSS_WEIGHTS.get(name, 1.))
             self.keras_model.add_loss(loss)
-	      log("add L2 regularization")
+	log("add L2 regularization")
         # Add L2 Regularization
         # Skip gamma and beta weights of batch normalization layers.
         reg_losses = [
@@ -2198,7 +2198,7 @@ class MaskRCNN():
             optimizer=optimizer,
             loss=[None] * len(self.keras_model.outputs))
 	    
-	      log("add metrics for losses")
+        log("add metrics for losses")
         # Add metrics for losses
         for name in loss_names:
             if name in self.keras_model.metrics_names:
@@ -2365,7 +2365,7 @@ class MaskRCNN():
         log("Checkpoint Path: {}".format(self.checkpoint_path))
         self.set_trainable(layers)
         self.compile(learning_rate, self.config.LEARNING_MOMENTUM)
-	      log("model compiled")
+	log("model compiled")
         # Work-around for Windows: Keras fails on Windows when using
         # multiprocessing workers. See discussion here:
         # https://github.com/matterport/Mask_RCNN/issues/13#issuecomment-353124009
@@ -2373,7 +2373,7 @@ class MaskRCNN():
             workers = 0
         else:
             workers = multiprocessing.cpu_count()
-	      log("starting training")
+	log("starting training")
         self.keras_model.fit_generator(
             train_generator,
             initial_epoch=self.epoch,
@@ -2387,7 +2387,7 @@ class MaskRCNN():
             use_multiprocessing=True,
 	          verbose=2
         )
-	      log("completed training")
+	log("completed training")
         self.epoch = max(self.epoch, epochs)
 
     def mold_inputs(self, images):
